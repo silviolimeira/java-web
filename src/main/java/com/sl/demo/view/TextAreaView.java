feafa0d287559fd7a8fd8c2263439271d361e325
Silvio <silvio.limeria@live.com>
2024-12-06 15:50:01 -0300

    add TextArea example

--- src/main/java/com/sl/demo/view/TextAreaView.java
+++ src/main/java/com/sl/demo/view/TextAreaView.java
@@ -9,6 +9,7 @@ import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.data.binder.ValidationException;
+import com.vaadin.flow.data.value.ValueChangeMode;
 
 public class TextAreaView extends Div {
 	
@@ -22,6 +23,13 @@ public class TextAreaView extends Div {
 		textArea = new TextArea();
 		textArea.setLabel("Description");
 		textArea.setValue("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
+		textArea.setHeight("300px");
+		textArea.setWidth("600px");
+		textArea.setMaxLength(10);
+		textArea.setValueChangeMode(ValueChangeMode.LAZY);
+		textArea.addValueChangeListener(event -> {
+			System.out.println("Something has been changed");
+		});
 
 		VerticalLayout form = new VerticalLayout();
 		form.add(title, textArea);
