122924517eaf6f50d925cc79bbea216c71b1cb64
Silvio <silvio.limeria@live.com>
2024-12-06 17:03:04 -0300

    add Grid example with multiple selection

--- src/main/java/com/sl/demo/view/GridView.java
+++ src/main/java/com/sl/demo/view/GridView.java
@@ -1,10 +1,12 @@
 package com.sl.demo.view;
 
 import com.sl.demo.model.Person;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.checkbox.Checkbox;
 import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.grid.ColumnTextAlign;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.grid.GridVariant;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -31,16 +33,33 @@ public class GridView extends Div {
 		employees.add(new Person("Julinana", "juliana@gmail.com", 47));
 
 		grid = new Grid<Person>(Person.class, false);
-		grid.addColumn(Person::getName).setHeader("Name").setTextAlign(ColumnTextAlign.START);
-		grid.addColumn(Person::getEmail).setHeader("Email").setTextAlign(ColumnTextAlign.CENTER);
-		grid.addColumn(Person::getAge).setHeader("Age").setTextAlign(ColumnTextAlign.END);
+		grid.addColumn(Person::getName).setHeader("Name")
+				.setTextAlign(ColumnTextAlign.START)
+				.setSortable(true);
+		grid.addColumn(Person::getEmail)
+				.setHeader("Email")
+				.setTextAlign(ColumnTextAlign.CENTER)
+				.setSortable(true);
+		grid.addColumn(Person::getAge)
+				.setHeader("Age")
+				.setTextAlign(ColumnTextAlign.END)
+				.setSortable(true);
 		grid.setWidth("600px");
 		grid.setItems(employees);
 		grid.setAllRowsVisible(true);
+		grid.addThemeVariants(GridVariant.LUMO_ROW_STRIPES);
+		grid.setSelectionMode(Grid.SelectionMode.MULTI);
 
+		Button button = new Button("Remove");
+		button.addClickListener(event -> {
+			System.out.println(grid.getSelectedItems());
+			employees.removeAll(grid.getSelectedItems());
+			grid.getDataProvider().refreshAll();
+			System.out.println(employees.size());
+		});
 
 		VerticalLayout form = new VerticalLayout();
-		form.add(title, grid);
+		form.add(title, button, grid);
 		add(form);
 
 	}
